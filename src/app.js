6506d6a3c618607f3349511061ff3ca941d5c2a6
aespinet <aelluvetaa@gmail.com>
2018-05-12 20:21:31 -0400

    Boilterplate

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router-dom';
+
+import store from './redux/store/store';
+import App, { history } from './components/App';
+
+
+const jsx = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route to="/" component={App} exact={true} />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render( jsx, document.getElementById('app') );
